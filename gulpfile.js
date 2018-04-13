018cc9adbe7fc577f7a77973fa8cd2bf88f369e2
Dalius Pamparas <dalius.pamparas@gmail.com>
2018-04-13 17:22:42 +0200

    live reload working

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
-var gulp = require('gulp'),
+var gulp = require('gulp');
     sass = require('gulp-sass'); 
+    browserSync = require('browser-sync');
 
-gulp.task('sass', function() { 
-  return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) 
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('css')) 
+gulp.task('sass', function(){
+      return gulp.src('app/sass/**/*.sass') // take all sass files
+          .pipe(sass()) // sass to css
+          .pipe(gulp.dest('app/css')) //send to map app/css
+          .pipe(browserSync.reload({stream: true})) // reload css with changes
   });
-
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('app/sass/**/*.sass', ['sass']); // wach if we have changes in sass files
+  
 });
 
-gulp.task('default', ['watch']);
+gulp.task('browser-sync', function() { // create task sync
+  browserSync({ 
+      server: { // options for server
+          baseDir: 'app' // course dir - app
+      },
+      notify: false 
+  });
+});
